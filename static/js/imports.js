5906157385beaf4f76a4836fc3f0d1361eb0061d
LeydenJar <hx.luan128@gmail.com>
2020-01-19 16:09:57 -0300

    made static files better

---
+++ static/js/imports.js
@@ -0,0 +1,7 @@
+import '../css/index.scss';
+import '../css/backbutton.css';
+import '../css/carousel.css';
+import '../css/release.css';
+import '../css/social.css';
+import '../css/sprite.css';
+import '../js/index.js';
